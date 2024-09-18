ba0967210c328421daf904482530635ae52a2050
Avesh6754 <aweshprajapati150@gmail.com>
2024-09-18 17:10:40 +0530

    18/9/24

--- 2.dart
+++ 2.dart
@@ -1,20 +1,24 @@
-class car
-{
+class car {
+  String? _model, _year;
+  int? _speed;
 
-   String? _model,_year;
-   int? _speed;
+  car(String model, year, int speed) {
+    this._model = model;
+    this._year = year;
+    this._speed = speed;
+  }
   int? get getcarspeed => _speed;
   String? get getcarmodel => _model;
-  String?  get getcaryear => _year;
-  
-car( String model,year , int speed)
-{
-  this._model = model;
-  this._year = year;
-  this._speed = speed;
+  String? get getcaryear => _year;
+
+  carout() {
+    print("the car modal is $getcarmodel");
+    print("the car year is $getcaryear");
+    print("the car speed is $getcarspeed");
+  }
 }
 
-
-
-
+void main() {
+  var carBmw = new car("BMW M4 ", "2019", 400);
+  carBmw.carout();
 }
